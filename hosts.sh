5b424138a47916898e40bb5c2dd82e2c3e00185e
Haroen Viaene <hello@haroen.me>
2015-10-21 14:44:13 +0200

    hosts

---
+++ hosts.sh
@@ -0,0 +1,9 @@
+# add disallowed hosts to /etc/hosts
+# run with super user permissions
+
+echo "127.0.0.1 mycustomersupport365.com
+127.0.0.1 mackeeper.com
+127.0.0.1 macsecurityalert.net
+127.0.0.1 systemupdatenow.com
+127.0.0.1 urgent-computer-alert.com
+127.0.0.1 testpc24.theupgradeweb.com" >> /etc/hosts
