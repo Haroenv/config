2db545453f499d4940cae655e3cabb75b5351928
Haroen Viaene <hello@haroen.me>
2015-10-22 18:19:49 +0200

    ad

--- hosts.sh
+++ hosts.sh
@@ -6,4 +6,5 @@ echo "127.0.0.1 mycustomersupport365.com
 127.0.0.1 macsecurityalert.net
 127.0.0.1 systemupdatenow.com
 127.0.0.1 urgent-computer-alert.com
+127.0.0.1 paid.outbrain.com
 127.0.0.1 testpc24.theupgradeweb.com" >> /etc/hosts
