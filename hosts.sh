c2954ff24668987ba6d8da3277558d0d49d7d17e
Haroen Viaene <hello@haroen.me>
2015-12-29 18:46:17 +0100

    more, or do I mean less? malware

--- hosts.sh
+++ hosts.sh
@@ -1,10 +1,15 @@
 # add disallowed hosts to /etc/hosts
 # run with super user permissions
 
-echo "127.0.0.1 mycustomersupport365.com
+echo "# malware and ads
+127.0.0.1 mycustomersupport365.com
 127.0.0.1 mackeeper.com
+127.0.0.1 mackeeperapp.mackeeper.com
+127.0.0.1 mackeeperapp2.mackeeper.com
+127.0.0.1 mackeeperapp3.mackeeper.com
 127.0.0.1 macsecurityalert.net
 127.0.0.1 systemupdatenow.com
 127.0.0.1 urgent-computer-alert.com
+127.0.0.1 testpc24.theupgradeweb.com
 127.0.0.1 paid.outbrain.com
-127.0.0.1 testpc24.theupgradeweb.com" >> /etc/hosts
+127.0.0.1 static.6min.today" >> /etc/hosts
