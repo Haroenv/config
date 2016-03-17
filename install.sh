d9aefea1ba4b590fb024212fa12402c6ce86e1cc
Haroen Viaene <fingebimus@me.com>
2016-03-17 10:45:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 # install usual installs I do #
 ###############################
-brew install bash-completion curl gdbm libyaml openssl readline screenfetch unar brew-cask emacs grc node python ruby sqlite wget
+brew install bash-completion curl gdbm libyaml openssl readline screenfetch unar brew-cask emacs grc node python ruby sqlite wget fish
+
+# set up fish #
+###############
+echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
 
 # install gem #
 ###############
