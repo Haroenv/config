f086169e29e14cd1f5886b64069f50d0fea30c41
Haroen Viaene <hello@haroen.me>
2017-02-03 15:23:56 +0100

    el aweosoms meims le xd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,9 +18,11 @@ alias c='clear'
 alias vi='vim'
 alias ping='ping -c 5'
 alias top='htop'
+alias code='code-insiders'
 
 # git aliases #
 ###############
+alias git='hub'
 alias g='git'
 alias gca='git commit -am'
 alias gps='git push'
@@ -29,6 +31,7 @@ alias gall='git add .'
 alias gis='git status'
 alias ga='git add'
 alias gco='git checkout'
+alias gbpurge='git branch --merged | grep -Ev "(\*|master|develop|staging)" | xargs -n 1 git branch -d'
 
 # ls after a cd #
 #################
@@ -55,3 +58,22 @@ alias template='curl -s https://raw.githubusercontent.com/Haroenv/simple-jekyll/
 # brew install thefuck #
 ########################
 eval (thefuck --alias | tr '\n' ';')
+
+# jekyll serve secure #
+# requires ca         #
+#######################
+function jess
+  open https://localhost:4000
+  jekyll s --ssl-cert ca.crt --ssl-key ca.key
+end
+function jes
+  open http://localhost:4000
+  jekyll serve
+end
+
+# update github pages          #
+# application type will change #
+################################
+function update-github
+  curl -v -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.mister-fantastic-preview+json" "https://api.github.com/repos/$argv/pages/builds"
+en
